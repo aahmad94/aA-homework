5336f47275922ec367469f472bd017a61ddddd60
aahmad94 <aka5vm@virginia.edu>
2017-11-13 22:31:23 -0800

    add intro to middleware hw

---
+++ W7D2/intro_to_middleware/frontend/store/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+import addLoggingToDispatch from '../todo_redux';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    applyMiddleware(addLoggingToDispatch));
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+  return store;
+};
+
+export default configureStore;
