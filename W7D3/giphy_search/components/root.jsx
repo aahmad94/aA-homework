38b2f247f9c5803bddd49f6e73f326e44e48cad0
aahmad94 <aka5vm@virginia.edu>
2017-11-15 02:44:41 -0800

    add giphy hw w api ajax call functionality

---
+++ W7D3/giphy_search/components/root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import GiphysSearchContainer from './giphys_search_container';
+
+const Root =  ( { store }) => (
+  <provider store={ store }>
+
+  </provider>
+);
